368844f38afac41772cce53fde8f3139f6693e38
unknown <fernandoptrentin@gmail.com>
2020-03-24 18:42:55 -0300

    alteração no front end, mensagem inicial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Omni Stack
+          Hello Treta
         </p>
         <a
           className="App-link"
